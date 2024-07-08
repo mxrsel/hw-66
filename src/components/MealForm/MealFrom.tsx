8ae02f1e7781de18b828ab25366b7e0fbe301960
mxrsel <marsel14082006@gmail.com>
2024-07-08 15:48:33 +0600

    added html content in meal form

--- src/components/MealForm/MealFrom.tsx
+++ src/components/MealForm/MealFrom.tsx
@@ -1,8 +1,47 @@
 
 const MealFrom = () => {
   return (
-    <div>
-meal form
+    <div className="row mt-3">
+      <form>
+        <div className="form-group">
+          <label>
+            Time:
+          </label>
+          <select
+            name="time"
+            required
+            className="form-select bg-dark text-light"
+            aria-label="Disabled select example">
+            <option value="Breakfast">Breakfast</option>
+            <option value="Snack">Snack</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+          </select>
+        </div>
+
+        <label>
+          Description:
+        </label>
+        <input
+          type="text"
+          name="title"
+          required
+          className="form-control bg-dark text-light"
+        />
+
+        <label>
+          Calories:
+        </label>
+        <input
+          type="number"
+          name="calories"
+          required
+          className="form-control bg-dark text-light"
+        />
+
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </form>
     </div>
   );
 };
