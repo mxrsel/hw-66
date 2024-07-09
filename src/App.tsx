ecad2f895cd748173dd5fcd2b94ff44259212660
mxrsel <marsel14082006@gmail.com>
2024-07-09 13:57:00 +0600

    added function for count calories

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ const App: React.FC= () => {
 
   const [meals, setMeals] = useState<Meal[]>([]);
   const [loading, setLoading] = useState(false);
-const navigate = useNavigate();
+  const [totalCalories, setTotalCalories] = useState(0);
+  const navigate = useNavigate();
 
   const fetchMeals = useCallback(async () => {
     try {
@@ -19,6 +20,7 @@ const navigate = useNavigate();
 
       if(!meals) {
         setMeals([]);
+        setTotalCalories(0);
       }else {
         const newMeals = Object.keys(meals)
           .map((id) => ({
@@ -26,6 +28,7 @@ const navigate = useNavigate();
           id,
         }));
         setMeals(newMeals);
+        setTotalCalories(newMeals.reduce((sum, meal) => sum + meal.calories, 0));
       }
     }finally {
       setLoading(false);
@@ -46,27 +49,39 @@ const navigate = useNavigate();
     <>
     <header>
       <div className="body">
-      <Routes>
-        <Route path="/" element={<Header/>}/>
-      </Routes>
+        <Header />
       </div>
     </header>
 
   <main className="container-fluid">
     <div className="header-main d-flex justify-content-around border-bottom-1">
-   <p className="calories-counter">
-     Total Calories: 900kcal
-   </p>
+      <p className="calories-counter">
+        Total Calories: {totalCalories}
+      </p>
+
+      <ul className="d-flex">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          meals.map(meal => (
+            <li key={meal.id}>
+              <span className="text-bg-secondary">{meal.time}</span>
+              <span>{meal.description}</span>
+              <span>{meal.calories}</span>
+            </li>
+          ))
+        )}
+      </ul>
 
       <button onClick={() => navigate('/mealAdd')}>Add New Meal</button>
     </div>
 
     <Routes>
-      <Route path="/mealAdd" element={<MealForm onSubmit={addMeal} /> }/>
+      <Route path="/mealAdd" element={<MealForm onSubmit={addMeal}/>}/>
     </Routes>
   </main>
-</>
-)
+    </>
+  )
 };
 
 export default App;
