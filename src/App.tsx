45778ef318057c05cb7d187018ebbc4d3f34f296
mxrsel <marsel14082006@gmail.com>
2024-07-09 15:09:37 +0600

    added function for editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,99 @@
-import {Route, Routes, useNavigate} from 'react-router-dom';
-import Header from './containers/Header/Header';
-import React, {useCallback, useState} from 'react';
-import MealForm from './components/MealForm/MealFrom';
-import {Meal} from './types';
+import { useCallback, useEffect, useState } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { Meal } from './types';
 import axiosApi from './axiosApi';
-import axios from 'axios';
+import MealForm from './components/MealForm/MealFrom';
+import Header from './containers/Header/Header';
 
-const App: React.FC= () => {
-
+const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const [meals, setMeals] = useState<Meal[]>([]);
   const [loading, setLoading] = useState(false);
   const [totalCalories, setTotalCalories] = useState(0);
-  const navigate = useNavigate();
 
   const fetchMeals = useCallback(async () => {
     try {
       setLoading(true);
-      const {data: meals} = await axios.get('/meals.json');
-
-      if(!meals) {
+      const { data: meals } = await axiosApi.get('/meals.json');
+      if (!meals) {
         setMeals([]);
         setTotalCalories(0);
-      }else {
-        const newMeals = Object.keys(meals)
-          .map((id) => ({
+      } else {
+        const newMeals = Object.keys(meals).map((id) => ({
           ...meals[id],
           id,
         }));
         setMeals(newMeals);
         setTotalCalories(newMeals.reduce((sum, meal) => sum + meal.calories, 0));
       }
-    }finally {
+    } finally {
       setLoading(false);
     }
   }, []);
 
+  useEffect(() => {
+    if (location.pathname === '/') {
+      void fetchMeals();
+    }
+  }, [fetchMeals, location]);
+
   const addMeal = async (meal: Meal) => {
+    setLoading(true);
     try {
       await axiosApi.post('/meals.json', meal);
       void fetchMeals();
-    }catch (error) {
+      navigate('/');
+    } catch (error) {
       console.error(error);
-    }finally {
+    } finally {
       setLoading(false);
     }
   };
+
+  const editMeal = async (meal: Meal) => {
+    if (!meal.id) return;
+    setLoading(true);
+    try {
+      await axiosApi.put(`/meals/${meal.id}.json`, meal);
+      void fetchMeals();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
     <header>
-      <div className="body">
-        <Header />
-      </div>
+      <Header />
     </header>
-
-  <main className="container-fluid">
-    <div className="header-main d-flex justify-content-around border-bottom-1">
-      <p className="calories-counter">
-        Total Calories: {totalCalories}
-      </p>
-
-      <ul className="d-flex">
+    <main className="container-fluid">
+      <button onClick={() => navigate('/add')}>Add Meal</button>
+      <p>Total Calories: {totalCalories}</p>
+      <ul>
         {loading ? (
           <p>Loading...</p>
         ) : (
           meals.map(meal => (
             <li key={meal.id}>
-              <span className="text-bg-secondary">{meal.time}</span>
-              <span>{meal.description}</span>
-              <span>{meal.calories}</span>
+              <span className="d-flex flex-column align-items-center">
+                {meal.time} -
+                {meal.description} -
+                {meal.calories} kcal</span>
+              <button onClick={() => navigate(`/edit/${meal.id}`)}>Edit</button>
             </li>
           ))
         )}
       </ul>
-
-      <button onClick={() => navigate('/mealAdd')}>Add New Meal</button>
-    </div>
-
-    <Routes>
-      <Route path="/mealAdd" element={<MealForm onSubmit={addMeal}/>}/>
-    </Routes>
-  </main>
+      <Routes>
+        <Route path="/add" element={<MealForm onSubmit={addMeal} />} />
+        <Route path="/edit/:id" element={<MealForm onSubmit={editMeal} />} />
+      </Routes>
+    </main>
     </>
-  )
+  );
 };
 
 export default App;
